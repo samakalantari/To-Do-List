fcc5d4bd8cf3e5a5b2d7a759106d13a563268b00
sama <sama@sotoon.ir>
2023-02-09 14:44:09 +0330

    fix path names

--- app.py
+++ app.py
@@ -18,62 +18,56 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-@app.route('/add_task', methods=["POST"])
+@app.route('/task', methods=["POST"])
 def add_task():
-    if request.method == "POST":
-        if not request.form.get("name"):
-            return {"error": "task must have name"}
+    if not request.form.get("name"):
+        return {"error": "task must have name"}
 
-        exists = db.session.query(Tasks.id).filter_by(name=request.form["name"]).first() is not None
-        if not exists:
-            task = Tasks(name=request.form.get("name"),
-                         desc=request.form.get("description"),
-                         status=request.form.get("status", "TODO"))
-            db.session.add(task)
-            db.session.commit()
-            return {"task saved successfully": task.name}
+    exists = db.session.query(Tasks.id).filter_by(name=request.form["name"]).first() is not None
+    if not exists:
+        task = Tasks(name=request.form.get("name"),
+                     desc=request.form.get("description"),
+                     status=request.form.get("status", "TODO"))
+        if task.status is "":
+            task.status = "TODO"
+        db.session.add(task)
+        db.session.commit()
+        return {"task saved successfully": task.name}
 
-        return {"error": "task already exists"}
+    return {"error": "task already exists"}
 
 
-@app.route('/remove_task', methods=["POST"])
+@app.route('/task', methods=["DELETE"])
 def remove_task():
-    if request.method == "POST":
-        if not request.form.get("name"):
-            return {"error": "which one?"}
+    if not request.form.get("name"):
+        return {"error": "which one?"}
 
-        exists = db.session.query(Tasks.id).filter_by(name=request.form["name"]).first() is not None
-        deleted = db.session.query(Tasks).filter_by(name=request.form["name"]).first().is_deleted is True
-        print(deleted)
-        if exists and not deleted:
-            task = db.session.query(Tasks).filter_by(name=request.form["name"]).first()
-            task.is_deleted = True
-            db.session.add(task)
-            db.session.commit()
-            return {"task deleted successfully": task.name}
-        elif exists and deleted:
-            return {"task already deleted": request.form.get("name")}
-        return {"error": "task {} not found".format(request.form.get("name"))}
+    exists = db.session.query(Tasks.id).filter_by(name=request.form["name"]).first() is not None
+    deleted = db.session.query(Tasks).filter_by(name=request.form["name"]).first().is_deleted is True
+    print(deleted)
+    if exists and not deleted:
+        task = db.session.query(Tasks).filter_by(name=request.form["name"]).first()
+        task.is_deleted = True
+        db.session.add(task)
+        db.session.commit()
+        return {"task deleted successfully": task.name}
+    elif exists and deleted:
+        return {"task already deleted": request.form.get("name")}
+    return {"error": "task {} not found".format(request.form.get("name"))}
 
 
-@app.route('/all_tasks', methods=["POST"])
+@app.route('/tasks', methods=["GET"])
 def all_tasks():
-    if request.method == "POST":
-        data = db.session.query(Tasks).all()
-        tasks = pd.DataFrame()
-        for row in data:
-            task = {
-                "name": row.name,
-                "description": row.desc,
-                "status": row.status.value
-            }
-            tasks = tasks.append(task, ignore_index=True)
-        tasks = tasks.to_json(orient="split")
+    data = db.session.query(Tasks).all()
+    tasks = pd.DataFrame()
+    for row in data:
+        task = {
+            "name": row.name,
+            "description": row.desc,
+            "status": row.status.value
+        }
+        tasks = tasks.append(task, ignore_index=True)
+    tasks = tasks.to_json(orient="split")
 
     return "Not exists" if None else tasks
 
